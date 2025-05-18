b51f190bc5d62956765a0b8bb9a2460111c1a58b
Christian Boulanger <info@bibliograph.org>
2025-05-18 22:02:54 +0200

    BUG: draggable flyover not working properly Fixes #7

---
+++ bin/generate-importmap.js
@@ -0,0 +1,22 @@
+import { writeImportmaps } from "@jsenv/importmap-node-module";
+
+const directoryUrl = new URL("./..", import.meta.url);
+
+// @ts-ignore
+await writeImportmaps({
+  directoryUrl,
+  importmaps: {
+    "./app/web/importmap2.json": {
+      importResolution: {
+        entryPoints: ["./app/src/app.js", "./app/src/ui.js"],
+      }
+    }
+  },
+  packagesManualOverrides: {
+    "react-redux": {
+      exports: {
+        import: "./es/index.js",
+      }
+    }
+  }
+});
