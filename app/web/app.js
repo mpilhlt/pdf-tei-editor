64d6ba0433247529b57e992aad4f0d3fc35139ca
Christian Boulanger <info@bibliograph.org>
2025-08-23 18:59:29 +0200

    Update build files

--- app/web/app.js
+++ app/web/app.js
@@ -14699,6 +14699,9 @@ function updateUi() {
 updateUi();
 var ui$1 = ui;
 
+// @ts-ignore
+window.ui = ui; // for debugging
+
 /**
  * This application plugin implements a dialog registered as the "diaolog" property of the app
  */
@@ -59233,6 +59236,7 @@ MarkdownIt.prototype.renderInline = function (src, env) {
 const api$3 = {
   open,
   load,
+  goBack,
   close
 };
 
@@ -59253,16 +59257,19 @@ const plugin$6 = {
  * @typedef {object} infoDialogPart
  * @property {SlDialog} self
  * @property {HTMLDivElement} content
+ * @property {SlButton} backBtn
  * @property {SlButton} closeBtn
  */
 
 // editor dialog
 const infoHtml = `
 <sl-dialog name="infoDialog" label="Information" class="dialog-big">
-  <div>
-    <div name="content"></div>
-    <sl-button name="closeBtn" slot="footer" variant="primary">Close</sl-button>
-  <div>
+  <div name="content"></div>
+  <sl-button name="backBtn" slot="footer" variant="default" disabled>
+    <sl-icon name="arrow-left"></sl-icon>
+    Back
+  </sl-button>
+  <sl-button name="closeBtn" slot="footer" variant="primary">Close</sl-button>
 </sl-dialog>
 `;
 
@@ -59291,6 +59298,12 @@ let md;
 const docsBasePath = "../../docs";
 
 /**
+ * Navigation history for the back button
+ * @type {string[]}
+ */
+let navigationHistory = [];
+
+/**
  * Runs when the main app starts so the plugins can register the app components they supply
  * @param {ApplicationState} state The main application
  */
@@ -59299,6 +59312,7 @@ async function install$6(state) {
   // add the component html
   await createHtmlElements(infoHtml, document.body);
   ui$1.infoDialog.closeBtn.addEventListener('click', () => ui$1.infoDialog.hide());
+  ui$1.infoDialog.backBtn.addEventListener('click', goBack);
 
   // add a button to the command bar to show dialog with prompt editor
   const button = (await createHtmlElements(buttonHtml))[0];
@@ -59316,24 +59330,35 @@ async function install$6(state) {
   
   // @ts-ignore
   window.appInfo = api$3;
-  load('index.md');
 }
 
 // API
 
 /**
- * Opens the prompt editor dialog
+ * Opens the info dialog
  * todo this needs to always reload the data since it might have changed on the server
  */
 async function open() {
+  // Reset navigation history when opening the dialog
+  navigationHistory = [];
+  updateBackButton();
   ui$1.infoDialog.show();
+  // Load the index page
+  await load('index.md');
 }
 
 /**
  * Loads markdowm and converts it to HTML, replacing links to local content to calls to this method
  * @param {string} mdPath The local path to the md file, relative to the "docs" dir
+ * @param {boolean} addToHistory Whether to add this page to navigation history (default: true)
  */
-async function load(mdPath){
+async function load(mdPath, addToHistory = true){
+  // Add current page to history if we're navigating to a new page
+  if (addToHistory && navigationHistory.length === 0 || navigationHistory[navigationHistory.length - 1] !== mdPath) {
+    navigationHistory.push(mdPath);
+    updateBackButton();
+  }
+  
   // remove existing content
   ui$1.infoDialog.content.innerHTML = "";
   
@@ -59351,7 +59376,7 @@ async function load(mdPath){
     // replace local links with api calls
     .replaceAll(
       /(<a\s+.*?)href=(["'])((?!https?:\/\/|\/\/|#|mailto:|tel:|data:).*?)\2(.*?>)/g, 
-      `$1href="#" onclick="appInfo.load('${docsBasePath}/$3'); return false"$4`
+      `$1href="#" onclick="appInfo.load('$3'); return false"$4`
     )
     // open remote links in new tabs
     .replaceAll(/(href="http)/g, `target="_blank" $1`);
@@ -59359,6 +59384,29 @@ async function load(mdPath){
   await createHtmlElements(html, ui$1.infoDialog.content);
 }
 
+
+/**
+ * Goes back to the previous page in navigation history
+ */
+function goBack() {
+  if (navigationHistory.length > 1) {
+    // Remove current page from history
+    navigationHistory.pop();
+    // Load the previous page without adding to history
+    const previousPage = navigationHistory[navigationHistory.length - 1];
+    navigationHistory.pop(); // Remove it so load() can add it back
+    load(previousPage);
+  }
+}
+
+/**
+ * Updates the back button state based on navigation history
+ */
+function updateBackButton() {
+  if (ui$1.infoDialog && ui$1.infoDialog.backBtn) {
+    ui$1.infoDialog.backBtn.disabled = navigationHistory.length <= 1;
+  }
+}
 
 /**
  * Closes the prompt editor
