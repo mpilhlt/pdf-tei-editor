d8d0fe8ee8179cb21b1be5ff00f42730f42e8c9a
Christian Boulanger <info@bibliograph.org>
2025-10-14 14:15:32 +0200

    Phase 6 wip

--- bin/test-fastapi.py
+++ bin/test-fastapi.py
@@ -43,7 +43,7 @@ class Colors:
         if platform.system() == 'Windows':
             try:
                 import ctypes
-                kernel32 = ctypes.windll.kernel32
+                kernel32 = ctypes.windll.kernel32 # type:ignore
                 kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
             except:
                 # If color initialization fails, disable colors
