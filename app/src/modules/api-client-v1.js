d6b53336182dfb47b6d5117720648e2d997a613e
cboulanger <info@bibliograph.org>
2025-10-17 16:11:17 +0200

    Phase 9 WIP

--- app/src/modules/api-client-v1.js
+++ app/src/modules/api-client-v1.js
@@ -1,7 +1,7 @@
 /**
  * Auto-generated API client for PDF-TEI Editor API v1
  *
- * Generated from OpenAPI schema at 2025-10-17T13:12:43.825Z
+ * Generated from OpenAPI schema at 2025-10-17T14:10:44.465Z
  *
  * DO NOT EDIT MANUALLY - regenerate using: npm run generate-client
  */
