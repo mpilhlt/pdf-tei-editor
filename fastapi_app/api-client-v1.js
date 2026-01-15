81f73b73e0185f7aad1ffe9dcf40f64cd13bde87
Christian Boulanger <info@bibliograph.org>
2026-01-15 09:58:51 +0100

    wip

--- fastapi_app/api-client-v1.js
+++ fastapi_app/api-client-v1.js
@@ -1,7 +1,7 @@
 /**
  * Auto-generated API client for PDF-TEI Editor API v1
  *
- * Generated from OpenAPI schema at 2026-01-15T09:24:26.970Z
+ * Generated from OpenAPI schema at 2026-01-15T08:50:29.302Z
  *
  * DO NOT EDIT MANUALLY - regenerate using: npm run generate-client
  */
@@ -245,6 +245,7 @@
  * @property {number} file_size
  * @property {string=} label
  * @property {string=} variant
+ * @property {string=} status
  * @property {number=} version
  * @property {boolean=} is_gold_standard
  * @property {Array<string>=} doc_collections
